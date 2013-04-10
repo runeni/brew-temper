f813c21676855d5814d28b0bcd42c02d6ef54939
runeni <rune@template.no>
2013-04-10 09:22:15 +0200

    Set content_type json

--- app.rb
+++ app.rb
@@ -6,15 +6,18 @@ set :database, "sqlite3:///temper.db"
 class Measure < ActiveRecord::Base
 end
 
-get '/measures' do
-  Measure.all.to_json
+get '/measures.json' do
+  content_type :json
+  Measure.where("created_at >= '2013-04-06 19:30'").all.to_json
 end
 
 get '/measure/:at' do
+  content_type :json
   Measure.find_by_created_at(params[:at]).to_json
 end
 
 get '/last.json' do
+  content_type :json
   Measure.last.to_json
 end
 
